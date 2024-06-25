0f957e50e80af45a9bcd3fd70fb9effe5f581952
Rafaelsn11 <faelsantana001@gmail.com>
2024-06-24 23:52:22 -0300

    feat: class CarrinhoItem

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EstudosPooCsharp.Models;
+
+// CarrinhoItem bola = new CarrinhoItem();
+
+// bola.nome = "Bola de Futebol";
+// bola.quantidade  = 1;
+// bola.preco = 99.00M;
+
+// CarrinhoItem camisa = new CarrinhoItem();
+
+// camisa.nome = "Camiseta de Frio";
+// camisa.quantidade  = 1;
+// camisa.preco = 99.00M;
+
+CarrinhoItem bola = new CarrinhoItem();
+bola.Nome = "Bola de Futebol";
+bola.Quantidade = 0;
